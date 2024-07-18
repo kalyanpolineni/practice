b02a024945aada7491c9a200f9a317449e35e22c
kalyanpolineni <kalyanpolineni@gmail.com>
2024-07-18 11:44:26 +0530

    added variable file

--- resource.tf
+++ resource.tf
@@ -1,3 +1,8 @@
 resource "aws_instance" "demo" {
-  
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "linuxkey"
+  tags = {
+    Name = "terraformserver"
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {
+  default = "ami-022ce6f32988af5fa"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
